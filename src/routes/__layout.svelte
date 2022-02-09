b3c093a87ea17f73f0ebe1f3939a2525993d60b0
TheOnlyTails <theonlytails@theonlytails.com>
2022-02-09 18:04:29 +0200

    Made the beep about 10x louder. Removed unused styles. Added a comment with a permalink to the audio generator. Updated deps.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,6 +3,8 @@
 	import { beepAudio } from "$lib"
 
 	onMount(() => {
+		// audio permalink:
+		// https://sfxr.me/#57uBnWkAfvbhTZQJAyEjTczWfcRpQqAS6rvcTvCsEwUeWsFRVdaFcrDvaUXT1r2H7jCSoH4vvCYWL3YGBJaeyXmoCHYZeM2m7ibTLWyH3isMv3KX8ny7MZRYT
 		beepAudio.beep = new Audio("/beep.wav")
 	})
 </script>
