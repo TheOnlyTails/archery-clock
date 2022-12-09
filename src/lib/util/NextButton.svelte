8a8d61b6b2de746ed79cb6cd67c3dfa65775365f
theonlytails <theonlytails@theonlytails.com>
2022-12-09 12:27:56 +0200

    i18n fixes

--- src/lib/util/NextButton.svelte
+++ src/lib/util/NextButton.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-  import { changeState } from "$lib/clock-state"
-  import { page } from "$app/stores"
-  import { Button } from "fluent-svelte"
-  import Play from "@fluentui/svg-icons/icons/play_16_regular.svg?raw"
-  import { _ } from "svelte-i18n"
-  import { beepAudio } from "$lib/audio"
+	import { changeState } from "$lib/clock-state"
+	import { page } from "$app/stores"
+	import { Button } from "fluent-svelte"
+	import Play from "@fluentui/svg-icons/icons/play_16_regular.svg?raw"
+	import { _ } from "svelte-i18n"
+	import { beepAudio } from "$lib/audio"
 
-  const handleClick = () => {
-    if ($page.route.id !== "/end") {
-      beepAudio.playBeep($page.route.id === "/" ? 2 : 1)
-      changeState($page)
-    }
-  }
+	const handleClick = () => {
+		if ($page.route.id !== "/end") {
+			beepAudio.playBeep($page.route.id === "/" ? 2 : 1)
+			changeState($page)
+		}
+	}
 </script>
 
 <Button class="next-button" on:click={handleClick} on:click variant="accent">
-  {@html Play} {$_(`next_button.${$page.route.id === "/" ? "start" : "continue"}`)}
+	{@html Play}
+	{#if $page.route.id === "/"}
+		{$_("next_button.start")}
+	{:else}
+		{$_("next_button.continue")}
+	{/if}
 </Button>
