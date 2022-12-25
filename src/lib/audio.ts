23c0aef74dbdbb8977b81b862998889bd6542d7c
theonlytails <theonlytails@theonlytails.com>
2022-12-25 10:33:43 +0200

    let's try this with tone.js

--- src/lib/audio.ts
+++ src/lib/audio.ts
@@ -1,24 +1,22 @@
 import { browser } from "$app/environment"
 import { beepVolume } from "$lib/settings"
 import { get } from "svelte/store"
+import { Synth, now } from "tone"
 
 export const playBeep = async (times = 1) => {
 	if (!browser) return
 
-	const REAL_TIME_FREQUENCY = 440
+	const synth = new Synth({
+		volume: 10 & Math.log10(get(beepVolume) / 100)
+	}).toDestination()
+	const time = now()
 
-	const audioContext = new AudioContext()
-
-	const oscillator = audioContext.createOscillator()
-	oscillator.frequency.value = REAL_TIME_FREQUENCY
-
-	const gain = audioContext.createGain()
-	gain.gain.setValueAtTime(get(beepVolume) / 100, audioContext.currentTime)
-
-	oscillator.connect(gain).connect(audioContext.destination)
+	const note = "C4"
 
 	for (let i = 0; i <= times; i++) {
-		oscillator.start()
-		oscillator.stop(audioContext.currentTime + 1)
+		// trigger the attack immediately
+		synth.triggerAttack(note, time + i)
+		// wait one second before triggering the release
+		synth.triggerRelease(time + i + 1)
 	}
 }
